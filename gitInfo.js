1bc9b686a5cbff48d4c8996f29ed0d2e44119fab
DaQuan Cartwright <daquancartwright@outlook.com>
2023-07-02 14:17:31 -0700

    Initial Commit

---
+++ gitInfo.js
@@ -0,0 +1,87 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitDefinition = 'Git is a distributed version control system (DVCS) used for tracking changes in files and coordinating work among multiple developers. It allows multiple people to collaborate on a project, making it easier to manage and merge changes efficiently.'
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = 'GitHub is a web-based hosting service for Git repositories. It provides a platform for developers to store, manage, and collaborate on their code projects using Git version control.'
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInitDefinition = 'The git init command is used to initialize a new Git repository in a directory. When you run git init, it sets up all the necessary files and data structures that Git needs to start tracking changes and managing versions within that directory.'
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = 'The git clone command is used to create a local copy of a remote Git repository. It enables you to download the entire repository, including all its files, branches, commit history, and other version control data.'
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = 'The git status command is used to display the current state of the Git repository. It provides information about the tracked and untracked files, the branch status, and the status of the staging area.'
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = 'The git add command is used to add changes or new files to the staging area in Git. It prepares the changes to be included in the next commit.'
+
+let gitAddCode = 'git add .'
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+let gitCommitDefinition = 'The git commit command is used to create a new commit in Git. A commit represents a snapshot of the project at a specific point in time and records the changes made to the files. '
+
+let gitCommitCode = 'git commit -m "Initial commit'
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = 'The git push command is used to upload local commits to a remote repository. It updates the remote repository with the latest commits from your local branch.'
---
+++ sdlc.js
@@ -0,0 +1,33 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// 1. Planning
+// 2. Analysis of Requirements
+// 3. Design
+// 4. Implementation
+// 5. Testing & Integration
+// 6. Maintain
+
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysis = "The phase where the team analyzes the planning of the app."
+
+const design = "The phase that creates a high-level system design based on the requirements. Can be represented using diagrams, such as flowcharts."
+
+const implementation = "This phase is where the software design is translated into code."
+
+const testing = "Phase where the testing team evaluates the developed product(s) in order to assess whether they meet the requirements specified in the planning phase."
+
+const maintain = "The phase that involves monitoring and supporting the sorftware in the production environment."
